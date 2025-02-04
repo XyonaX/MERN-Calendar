1d32f30f5b1ceeff2ad989da6a48170166e95bc0
Jonatan <jonatan.vargasportillo@gmail.com>
2025-02-04 13:42:24 -0300

    Se agregaron componentes, helpers,routers y auth

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,3 +1,56 @@
+import { CalendarEvent, Navbar } from "../";
+import { Calendar } from "react-big-calendar";
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { addHours } from "date-fns";
+import { localizer, getMessagesES } from "../../helpers";
+
+
+const events = [
+    {
+        title: "Cumpleaños del jefe",
+        notes: "Comprar el regalo",
+        start: new Date(),
+        end: addHours(new Date(), 2),
+        bgcolor: "#fafafa",
+        user: {
+            _id: "123",
+            name: "Jonatan",
+        },
+    },
+];
+
+const eventStyleGetter = (event, start, end, isSelected) => {
+
+    const style = {
+        backgroundColor: '#347CF7',
+        borderRadius: '0px',
+        opacity: 0.8,
+        color: 'white',
+    };
+
+    return {
+        style,
+    };
+}
+
 export const CalendarPage = () => {
-    return <div>CalendarPage</div>;
+    return (
+        <>
+            <Navbar />
+
+            <Calendar
+                culture="es"
+                localizer={localizer}
+                events={events}
+                startAccessor='start'
+                endAccessor={"end"}
+                style={{ height: 'calc( 100vh - 80px)' }}
+                messages={getMessagesES()}
+                eventPropGetter={eventStyleGetter}
+                components={{
+                    event: CalendarEvent,
+                }}
+            />
+        </>
+    );
 };
