a6c5dcd7dc4fd798559c2cac8a13408df054a673
Vargas Jonatan <jonatan.vargasportillo@gmail.com>
2025-02-25 09:07:57 -0300

    se crearon los store

---
+++ src/hooks/useCalendarStore.js
@@ -0,0 +1,42 @@
+import { useDispatch, useSelector } from "react-redux"
+import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from "../store/calendar/calendarSlice";
+
+export const useCalendarStore = () => {
+    const dispatch = useDispatch();
+    const {events, activeEvent} = useSelector((state) => state.calendar);
+    
+    const setActiveEvent = (calendarEvent) => {
+        dispatch(onSetActiveEvent(calendarEvent));
+    }
+
+    const startSavingEvent = async(calendarEvent) => {
+        //TODO: Llegar al backend
+
+        if(calendarEvent._id){
+            //Actualizar
+            dispatch(onUpdateEvent({...calendarEvent}));
+        }else{
+            //Agregar
+            dispatch(onAddNewEvent({...calendarEvent, _id: new Date().getTime()}));
+        }
+    }
+
+    const startDeletingEvent = () => {
+        dispatch(onDeleteEvent())
+    }
+
+    return {
+        //*Propiedades
+        events,
+        activeEvent,
+        hasEventSelected: !!activeEvent,
+
+        //*Metodos
+        setActiveEvent,
+        startSavingEvent,
+        startDeletingEvent
+        
+    }
+
+
+}
---
+++ src/store/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { uiSlice } from "./ui/uiSlice";
+import { calendarSlice } from "./calendar/calendarSlice";
+
+
+export const store = configureStore({
+    reducer: {
+        calendar: calendarSlice.reducer,
+        ui: uiSlice.reducer,
+    },
+    devTools: true,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false
+    })
+})
